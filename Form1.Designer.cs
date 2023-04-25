4547f4f4bc31ac42e5d3187221e6f52ac38919ce
mooooms <its.mammy0@gmail.com>
2023-04-25 20:38:54 +0300

    intial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿using System.Windows.Forms;
+
+namespace bloknot
+{
+    partial class Bloknot
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.File = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.Font = new System.Windows.Forms.ToolStripMenuItem();
+            this.ItalicFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.Standart = new System.Windows.Forms.ToolStripMenuItem();
+            this.Bold = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Menu,
+            this.Font,
+            this.ItalicFont,
+            this.Standart,
+            this.Bold,
+            this.toolStripMenuItem1,
+            this.aToolStripMenuItem,
+            this.aToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(464, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Menu
+            // 
+            this.Menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.File});
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(53, 20);
+            this.Menu.Text = "Меню";
+            // 
+            // File
+            // 
+            this.File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenFile,
+            this.SaveFile});
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(103, 22);
+            this.File.Text = "Файл";
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(133, 22);
+            this.OpenFile.Text = "Открыть";
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click_1);
+            // 
+            // SaveFile
+            // 
+            this.SaveFile.Name = "SaveFile";
+            this.SaveFile.Size = new System.Drawing.Size(133, 22);
+            this.SaveFile.Text = "Сохранить";
+            this.SaveFile.Click += new System.EventHandler(this.SaveFile_Click);
+            // 
+            // Font
+            // 
+            this.Font.Name = "Font";
+            this.Font.Size = new System.Drawing.Size(58, 20);
+            this.Font.Text = "Шрифт";
+            this.Font.Click += new System.EventHandler(this.Font_Click);
+            // 
+            // ItalicFont
+            // 
+            this.ItalicFont.Name = "ItalicFont";
+            this.ItalicFont.Size = new System.Drawing.Size(58, 20);
+            this.ItalicFont.Text = "Курсив";
+            this.ItalicFont.Click += new System.EventHandler(this.ItalicFont_Click);
+            // 
+            // Standart
+            // 
+            this.Standart.Name = "Standart";
+            this.Standart.Size = new System.Drawing.Size(74, 20);
+            this.Standart.Text = "Обычный";
+            // 
+            // Bold
+            // 
+            this.Bold.Name = "Bold";
+            this.Bold.Size = new System.Drawing.Size(67, 20);
+            this.Bold.Text = "Жирный";
+            this.Bold.Click += new System.EventHandler(this.Bold_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(45, 20);
+            this.toolStripMenuItem1.Text = "Цвет";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // aToolStripMenuItem
+            // 
+            this.aToolStripMenuItem.Name = "aToolStripMenuItem";
+            this.aToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.aToolStripMenuItem.Text = "+A";
+            this.aToolStripMenuItem.Click += new System.EventHandler(this.aToolStripMenuItem_Click);
+            // 
+            // aToolStripMenuItem1
+            // 
+            this.aToolStripMenuItem1.Name = "aToolStripMenuItem1";
+            this.aToolStripMenuItem1.Size = new System.Drawing.Size(32, 20);
+            this.aToolStripMenuItem1.Text = "-A";
+            this.aToolStripMenuItem1.Click += new System.EventHandler(this.aToolStripMenuItem1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(464, 426);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Bloknot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Bloknot";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem Menu;
+        private ToolStripMenuItem File;
+        private ToolStripMenuItem OpenFile;
+        private ToolStripMenuItem SaveFile;
+        private ToolStripMenuItem Font;
+        private ToolStripMenuItem ItalicFont;
+        private ToolStripMenuItem Standart;
+        private ToolStripMenuItem Bold;
+        private RichTextBox richTextBox1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem aToolStripMenuItem;
+        private ToolStripMenuItem aToolStripMenuItem1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace bloknot
+{
+    public partial class Bloknot : Form
+    {
+        public Bloknot() => InitializeComponent();
+
+        private void OpenFile_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog file_Dialog = new OpenFileDialog();
+            file_Dialog.Filter = "Текстовые файлы(*.rtf)|*.rtf";
+            if (file_Dialog.ShowDialog().Equals(DialogResult.OK))
+            {
+                richTextBox1.LoadFile(file_Dialog.FileName);
+            }
+        }
+
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog file_Dialog = new SaveFileDialog();
+            file_Dialog.Filter = "Текстовые файлы(*.rtf)|*.rtf";
+            if (file_Dialog.ShowDialog().Equals(DialogResult.OK))
+            {
+                richTextBox1.SaveFile(file_Dialog.FileName);
+            }
+        }
+
+        private void Font_Click(object sender, EventArgs e)
+        {
+            FontDialog font_Dialog = new FontDialog();
+            if (font_Dialog.ShowDialog().Equals(DialogResult.OK))
+            {
+                richTextBox1.SelectionFont = font_Dialog.Font;
+            }
+
+        }
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ColorDialog color_Dialog = new ColorDialog();
+            if (color_Dialog.ShowDialog().Equals(DialogResult.OK))
+            {
+                richTextBox1.SelectionColor = color_Dialog.Color;
+            }
+        }
+        bool bold = false;
+        private void Bold_Click(object sender, EventArgs e)
+        {
+            //bold = !bold;
+            var f = richTextBox1.SelectionFont;
+            richTextBox1.SelectionFont = new Font(f.FontFamily, f.Size,
+                bold ? FontStyle.Bold : FontStyle.Regular);
+        }
+        bool italic = false;
+        private void ItalicFont_Click(object sender, EventArgs e)
+        {
+            var f = richTextBox1.SelectionFont;
+            richTextBox1.SelectionFont = new Font(f.FontFamily, f.Size,
+                bold ? FontStyle.Bold : FontStyle.Italic);
+        }
+
+        private void aToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var f = richTextBox1.SelectionFont;
+            richTextBox1.SelectionFont = new Font(f.FontFamily, f.Size + 1, f.Style);
+        }
+
+        private void aToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            var f = richTextBox1.SelectionFont;
+            richTextBox1.SelectionFont = new Font(f.FontFamily, f.Size - 1, f.Style);
+        }
+    }
+    
+}
